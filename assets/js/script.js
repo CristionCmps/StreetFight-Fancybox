2343f2755c0517c7b95574a0b5d683b83521caf2
Cristioncmps <cristion.campos@opovodigital.com>
2024-06-27 15:21:16 -0300

    fix: Menu Mobile in Processing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
+// $(document).ready(function () {
+//     // ABRIR BOX MENU MOBILE
+//     $('.mobile-menu-icon').click(function () {
+//         $('.mobile-menu ul').toggleClass("active");
+//         console.log($('.mobile-menu ul'))
+//         console.log('Teste');
+//     });
+// });
+
 $(document).ready(function () {
     // ABRIR BOX MENU MOBILE
-    $('.mobile-menu-icon').click(function () {
-        $('.mobile-menu ul').toggleClass("active");
-        console.log($('.mobile-menu ul'))
-        console.log('Teste');
+    $('.mobile-menu').click(function() {
+        $('.container__menu--mobile').toggleClass('active');
     });
-});
+  });
