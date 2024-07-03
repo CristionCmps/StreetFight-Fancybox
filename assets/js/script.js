da24a95be8b562778e647ca12c26bbf9461a2e07
Cristion Campos <dig.cristioncampos@opovo.local>
2024-07-03 15:54:32 -0300

    fix: Quebra de medidas nas img das sessoões e Font-Size nos Títulos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,8 @@
-// $(document).ready(function () {
-//     // ABRIR BOX MENU MOBILE
-//     $('.mobile-menu-icon').click(function () {
-//         $('.mobile-menu ul').toggleClass("active");
-//         console.log($('.mobile-menu ul'))
-//         console.log('Teste');
-//     });
-// });
-
 $(document).ready(function () {
     // ABRIR BOX MENU MOBILE
-    $('.icone__sandwich').click(function() {
-        $('.nav__mobile').toggleClass('active');
+    $('.mobile-menu').click(function() {
+        $('.container__menu--mobile').toggleClass('active');
+        // console.log($('.mobile-menu ul'))
+        // console.log('Teste');
     });
   });
